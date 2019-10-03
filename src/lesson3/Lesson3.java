dfd8b6a8a111fda87fedea10d068f5838b0c23c9
Andrew Rorat <rorat6937@gmail.com>
2019-10-03 19:42:20 +0300

    practice

---
+++ src/lesson3/Lesson3.java
@@ -0,0 +1,11 @@
+package lesson3;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+        char ch = 0;
+        for (var i=0; i<=50; i++){
+            ch++;
+            System.out.println(ch);
+        }
+    }
+}
