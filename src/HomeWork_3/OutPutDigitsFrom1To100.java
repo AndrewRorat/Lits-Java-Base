0672a926375268ca1174eac4fe5200b1c0334cd4
Andrew Rorat <rorat6937@gmail.com>
2019-10-08 11:44:51 +0300

    Third Home work.

--- src/HomeWork_3/OutPutDigitsFrom1To100.java
+++ src/HomeWork_3/OutPutDigitsFrom1To100.java
@@ -4,7 +4,7 @@ package HomeWork_3;
     Вивести всі прості числа він 1 до 100
      */
 
-public class OutpPutDigitsFrom1To100 {
+public class OutPutDigitsFrom1To100 {
     public static void main(String[] args) {
         var numbers = 0;
         for (var i = 1; i <= 100; i++) {
